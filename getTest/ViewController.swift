3576cd70ad1844feae48648e98dc3ce15fa89f14
Mark Reidy <mreidy@usgs.gov>
2018-09-14 14:22:46 -0400

    filled in files from https://github.com/Firanus/ios-stanford-concentration/tree/27058e6dda931e36c6ca2994940e7ec5bec72e10/Concentration

--- getTest/ViewController.swift
+++ getTest/ViewController.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    lazy var game = Concentration(numberOfCardPairs: cardButtons.count)
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton)
+    {
+        if let index = cardButtons.index(of: sender) {
+            game.chooseCard(at: index)
+            updateViewFromModel()
+        } else {
+            print("This card is not part of the cards array")
+        }
     }
-
-
-
-
+    
+    func updateViewFromModel()
+    {
+        for index in cardButtons.indices {
+            let card = game.cards[index]
+            let button = cardButtons[index]
+            if card.isFaceUp {
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+            } else if card.isMatched {
+                button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0)
+                button.setTitle("", for: UIControlState.normal)
+            } else {
+                button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.setTitle("", for: UIControlState.normal)
+            }
+        }
+    }
+    
+    var emojiChoices = ["🦇","😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
+    }
 }
 
